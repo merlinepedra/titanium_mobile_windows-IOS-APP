19ae78f4a139ae011b227b0ae4ba4891111e3142
Christopher Williams <chris.a.williams@gmail.com>
2016-02-19 13:11:15 -0500

    Update win10.bat
    Try to update timestamp on unit_report before copying it.

--- Tools/Scripts/win10.bat
+++ Tools/Scripts/win10.bat
@@ -12,8 +12,12 @@ echo Building for Windows 10
 call node build.js -s 10.0 -m 14.0 -o WindowsStore-x86 -o WindowsStore-ARM
 SET LEVEL=%ERRORLEVEL%
 rmdir node_modules /Q /S
-cd ..\\..\\..
+cd ..
 
+echo Updating timestamp on fake junit report
+copy /b junit_report.xml +,,
+
+cd ..\\..
 copy Tools\\Scripts\\junit_report.xml dist\\junit_report.xml
 copy Tools\\Scripts\\junit_report.xml dist\\junit_report_10.xml
 
