da77de065027394b3c91ba5e7f8e8471c7865a25
Gary Mathews <gmathews@appcelerator.com>
2016-09-13 02:17:43 +0100

    [TIMOB-23869] Fix Ti.Media.takePicture() capabilities (#867)

--- cli/commands/_build/generate.js
+++ cli/commands/_build/generate.js
@@ -290,7 +290,7 @@ function generateCapabilities(target, capabilities, deviceCapabilities) {
 				deviceCapability: ['microphone', 'webcam']
 			},
 			'Media.takePicture': {
-				uapCapability: ['videosLibrary']
+				uapCapability: ['picturesLibrary', 'videosLibrary']
 			},
 			// Titanium.Media.AudioRecorder.*
 			'Media.AudioRecorder': {
